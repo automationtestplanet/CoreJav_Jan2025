3a2a9737e48a09310b57b6d59d46554442f17547
RAJU CHELLE <rajuchelle@gmail.com>
2025-02-14 11:14:09 +0530

    java class11 code

--- src/oops/inheritance/NewCalculator.java
+++ src/oops/inheritance/NewCalculator.java
@@ -9,7 +9,7 @@ public class NewCalculator extends Calculator {
 	public int addition(int a, int b, int c) {
 		return a + b + c;
 	}
-	
+
 	public int addition(int a, int b) {
 		return a + b + 100;
 	}
